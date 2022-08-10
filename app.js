0fbe8e03c47ee4f67d684d82f332c1fa4f8ae805
talgatKuansyh <104269866+talgatKuansyh@users.noreply.github.com>
2022-08-10 20:31:27 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,131 +1,130 @@
-const control = document.querySelector('#control')
-const reset = document.querySelector('#reset')
-const save = document.querySelector('#save');
-const close = document.querySelector('#close');
-const title = document.querySelector('#title')
-const wrapper = document.querySelector('#wrapper')
-const timer = document.querySelector('#timer')
-const click = document.querySelector('#click')
-const clock = document.querySelector('#clock')
-const done = document.querySelector('#done')
+const time = document.querySelector(".time")
+const title = document.querySelector('.title')
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const resumeBtn = document.querySelector('.resume')
+const focusTime = document.querySelector('.focus-time')
+const shortBreakTime = document.querySelector('.short-break-time')
+const longBreakTime = document.querySelector('.long-break-time')
+
+let timeLeft = focusTime.value * 60; /// left time is seconds
+let cicles = 0;
+let timerID = null;
 
 
-let cicles = 0
-let timerID = null
-let timeLeft = document.querySelector('#pomodoro').value * 60;
-secondsConvertor(timeLeft)
-                
-function secondsConvertor(t){
-    let minutes = Math.floor(t / 60);
-    let seconds = t % 60;
-    timer.innerHTML = minutes.toString().padStart(2, "0") + ':' + seconds.toString().padStart(2, "0");
+// convert timeLeft to minutes
+const secondsConvertor = () => {
+    minutes = Math.floor(timeLeft / 60);
+    seconds = timeLeft % 60;
+    time.innerHTML = `${(minutes).toString().padStart(2, "0")}:${(seconds).toString().padStart(2, "0")}`;
 }
 
-function buttonInterface() {
-    if(timerID === null){
-        control.innerHTML = 'Start';
-        control.classList.remove('stop')
-        control.classList.add('start')
+secondsConvertor() 
+
+// change mode when timeLeft is 0
+const changeMode = () => {
+    
+    const Mode = [
+        pomodoro = {
+            time: focusTime.value * 60,
+            title: 'Stay Focused'
+        }, 
+        
+        shortBreak = {
+            time: shortBreakTime.value * 60,
+            title: 'Short break'
+        },
+        
+        longBreak = {
+            time: longBreakTime.value * 60,
+            title: 'Long break'
+        }
+    ]
+
+    if (cicles === 0) {
+        title.innerHTML = pomodoro.title
+        timeLeft = pomodoro.time
+    } else if (cicles === 1) {
+        title.innerHTML = shortBreak.title
+        timeLeft = shortBreak.time
+    } else if (cicles === 2) {
+        title.innerHTML = pomodoro.title
+        timeLeft = pomodoro.time
+    } else if (cicles === 3) {
+        title.innerHTML = shortBreak.title
+        timeLeft = shortBreak.time
+    } else if (cicles === 4) {
+        title.innerHTML = pomodoro.title
+        timeLeft = pomodoro.time
+    } else if (cicles === 5) {
+        title.innerHTML = longBreak.title
+        timeLeft = longBreak.time
+        cicles = -1
+        pause()
+    }
+
+    secondsConvertor()
+}
+
+changeMode()
+
+// update interface of buttons
+const changeButtonStyle = () => {
+    if(timerID === null) {
+        startBtn.classList.add('start')
+        startBtn.classList.remove('pause')
+        startBtn.innerHTML = 'Start'
     } else {
-        control.innerHTML = 'Pause'
-        control.classList.remove('start')
-        control.classList.add('stop')
+        startBtn.classList.add('pause')
+        startBtn.classList.remove('start')
+        startBtn.innerHTML = 'Pause'
     }
 }
 
-function start(){
-    if (timeLeft === 0) return;
-    click.play()
+
+const showSettings = () => {
+    document.querySelector('.wrapper').classList.toggle('none')
+    document.querySelector('.settings-wrapper').classList.toggle('none')
+}
+
+
+const saveSettings = () => {
+    document.querySelector('.wrapper').classList.toggle('none')
+    document.querySelector('.settings-wrapper').classList.toggle('none')
+    pause()
+    changeMode()
+}
+
+startBtn.addEventListener('click', ()=> {
+    if (timerID === null) {
+        start()
+    } else {
+        pause()
+        startBtn.innerHTML = 'Resume'
+    }
+})
+
+const start = () => {
+    if (timeLeft === 0) return
+
     timerID = setInterval(() => {
-        clock.play()
         timeLeft--;
-        secondsConvertor(timeLeft);
-        if(timeLeft === 0) {
-            done.play()
-            cicles++;
-            stop();
+        secondsConvertor()
+        
+        if (timeLeft === 0) {
+            cicles++
+            pause()
+            changeButtonStyle()
             changeMode()
         }
-    }, 1000);
-    buttonInterface();
-}
+        
+    }, 1000)
+    
+    changeButtonStyle()
+} 
 
-function stop() {
-    clock.pause()
-    clearInterval(timerID);
+const pause = () => {
+    clearInterval(timerID)
     timerID = null;
-    buttonInterface();
+    changeButtonStyle()
 }
-
-control.addEventListener('click', () => {
-        click.play()
-    if(timerID === null) {
-        start();
-        // changeMode()
-    } else {
-        stop();
-        control.innerHTML = "Resume"
-        control.classList.add('resume')
-    }
-});
-
-function changeMode() {
-    if(cicles === 0){
-        // timeLeft = document.querySelector('#pomodoro').value * 60
-        title.innerHTML = 'Stay Focused'
-    }
-    if(cicles === 1){
-        timeLeft = document.querySelector('#short').value * 60
-        // secondsConvertor(timeLeft)
-        title.innerHTML = 'Have a short break'
-    }
-    if(cicles === 2){
-        timeLeft = document.querySelector('#pomodoro').value * 60
-        // secondsConvertor(timeLeft)
-        title.innerHTML = 'Stay Focused'
-    }
-    if(cicles === 3){
-        timeLeft = document.querySelector('#short').value * 60
-        // secondsConvertor(timeLeft)
-        title.innerHTML = 'Have a short break'
-    }
-    if(cicles === 4){
-        timeLeft = document.querySelector('#pomodoro').value * 60
-        // secondsConvertor(timeLeft)
-        title.innerHTML = 'Stay Focused'
-    }
-    if(cicles === 5){
-        timeLeft = document.querySelector('#long').value * 60
-        // secondsConvertor(timeLeft)S
-        title.innerHTML ="It's time to long break"
-    }
-    if(cicles === 6){
-        title.innerHTML ="Done!"
-        cicles = 0
-    }
-}
-
-reset.addEventListener('click', () => {
-    wrapper.classList.add('none');
-    popup.classList.remove('none');
-});
-
-close.addEventListener('click', () => {
-    wrapper.classList.remove('none');
-    popup.classList.add('none');
-});
-
-save.addEventListener('click', () => {
-    cicles = 0
-    click.play()
-    wrapper.classList.remove('none');
-    popup.classList.add('none');
-    timer.innerHTML =  document.querySelector('#pomodoro').value * 60
-    secondsConvertor(timer.innerHTML)
-    timeLeft = document.querySelector('#long').value * 60
-    timeLeft = document.querySelector('#short').value * 60
-    timeLeft = document.querySelector('#pomodoro').value * 60
-    changeMode()
-    stop()
-});
-
